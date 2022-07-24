22c8a58aad7855b43e2a11aa9323f55f2dd1c907
Santiago Moreira Palma <sanmopa2000@gmail.com>
2022-07-24 16:21:12 -0500

    asistente virtual v1

---
+++ amr.py
@@ -0,0 +1,115 @@
+from re import S
+import speech_recognition as sr
+import subprocess as sub
+import pyttsx3, pywhatkit, wikipedia, datetime, keyboard, colors, os
+from pygame import mixer 
+
+name ="amr"
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+sites = {
+        'google':'google.com',
+        'youtube':'youtube.com',
+        'facebook':'facebook.com',
+        'cursos':'freecodecamp.org/learn',
+        'udemy':'udemy.com'
+}
+
+files ={
+    'proyecto':'PRESENTACION_PROYECTO1.pdf'
+}
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    listener = sr.Recognizer()     
+    with sr.Microphone() as source:
+        print("Escuchando...")
+        listener.adjust_for_ambient_noise(source)
+        pc = listener.listen(source)
+
+    try:
+        rec = listener.recognize_google(pc, language="es")
+        rec = rec.lower()
+    except sr.UnknownValueError:
+        print("No te entendí, intenta de nuevo")
+        if name in rec:
+            rec = rec.replace(name, '')
+    return rec
+
+def run_amr():
+    while True:
+        try:
+            rec = listen()
+        except UnboundLocalError:
+            print("No te entendí, intenta de nuevo")
+            continue     
+        if 'reproduce' in rec:
+            music = rec.replace('reproduce', '')
+            print("Reproduciendo " + music)
+            talk("Reproduciendo " + music)
+            pywhatkit.playonyt(music)
+        elif 'busca' in rec:
+            search = rec.replace('busca', '')
+            wikipedia.set_lang("es")
+            wiki = wikipedia.summary(search, 1)
+            print(search +":"+ wiki)
+            talk(wiki)
+        elif 'alarma' in rec:
+            num = rec.replace('alarma', '')
+            num = num.strip()
+            talk("Alarma activida a las "+ num + "horas")
+            while True:
+                if datetime.datetime.now().strftime('%H:%M') == num:
+                    print("DESPIERTA")
+                    mixer.init()
+                    mixer.music.load("auronplay-alarma.mp3")
+                    mixer.music.play()
+                    if keyboard.read_key() == "s":
+                        mixer.music.stop()
+                        break
+        elif 'colores' in rec:
+            talk("Enseguida")
+            colors.capture()
+        elif 'abre' in rec:
+            for site in sites:
+                if site in rec:
+                    sub.call(f'start chrome.exe {sites[site]}', shell=True)
+                    talk(f'Abriendo {site}')
+        elif 'archivo' in rec:
+             for file in files:
+                if file in rec:
+                    sub.Popen([files[file]], shell=True)
+                    talk(f'Abriendo {file}')
+        elif 'escribe' in rec:
+            try:
+                with open("nota.txt", 'a') as f:
+                    write(f)
+            
+            except FileNotFoundError as e:
+                file = open("nota.txt",'a')
+                write(file)
+                
+        elif 'termina' in rec:
+                talk('Adios')
+                break 
+
+            
+def write(f):
+    talk("¿Qué quieres que escriba?")
+    rec_write = listen()
+    f.write(rec_write+ os.linesep)
+    f.close()
+    talk("Listo, puedes revisarlo")
+    sub.Popen("nota.txt", shell=True)
+
+if __name__ == "__main__":
+    run_amr()
+
+
